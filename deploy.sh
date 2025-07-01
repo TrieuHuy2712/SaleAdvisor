85fe4266def319550b9bdc7a8e7cfc23368a2859
TrieuHuy2712 <31106741+TrieuHuy2712@users.noreply.github.com>
2025-07-01 22:31:58 +0700

    Add execute permission check to deploy.sh
    Added a shebang and logic to automatically set execute permission on the script if not already set. Also included a step to revert local changes to the script after deployment.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Auto set execute permission if not yet
+if [ ! -x "$0" ]; then
+    echo ">>> Give permission for $0"
+    chmod +x "$0"
+fi
 VENV_DIR="venv"
 
 echo ">>> Pull latest code from Git..."
@@ -15,6 +22,9 @@ sudo systemctl restart chatbot.service
 
 echo ">>> Status chatbot.service:"
 sudo systemctl status chatbot.service
+
+# Revert local change to script
+sudo git checkout -- "$0"
 
 # if you want to run the chatbot service in the background, uncomment the line below
 # sudo cp sale_advisor_deploy.sh /SaleAdvisor/deploy.sh
